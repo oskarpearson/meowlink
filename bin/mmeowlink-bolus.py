f2f5843bf99d1d3aa1c13ed5e390104d8a6c2ce9
Gustavo <gustavillo@gmail.com>
2016-04-07 23:29:50 -0500

    WIP. Not working.

--- bin/mmeowlink-bolus.py
+++ bin/mmeowlink-bolus.py
@@ -5,18 +5,21 @@ from decocare import commands
 from decocare import lib
 from decocare.helpers import cli
 
+from mmeowlink.link_builder import LinkBuilder
+from mmeowlink.handlers.stick import Pump
 
-class BolusApp (cli.CommandApp):
+
+class BolusApp(cli.CommandApp):
     def customize_parser(self, parser):
-        parser.add_argument('--radio_type', dest='radio_type', default='subg_rfspy',
-                            choices=['mmcommander', 'subg_rfspy'])
-        parser.add_argument('--mmcommander', dest='radio_type', action='store_const', const='mmcommander')
-        parser.add_argument('--subg_rfspy', dest='radio_type', action='store_const', const='subg_rfspy')
         parser.add_argument('units', type=float, help="Amount of insulin to bolus.")
         group = parser.add_mutually_exclusive_group(required=True)
         group.add_argument('--515', dest='strokes_per_unit', action='store_const', const=10)
         group.add_argument('--554', dest='strokes_per_unit', action='store_const', const=40)
         group.add_argument('--strokes', dest='strokes_per_unit', type=int)
+        parser.add_argument('--radio_type', dest='radio_type', default='subg_rfspy', choices=['mmcommander', 'subg_rfspy'])
+        parser.add_argument('--mmcommander', dest='radio_type', action='store_const', const='mmcommander')
+        parser.add_argument('--subg_rfspy', dest='radio_type', action='store_const', const='subg_rfspy')
+
         return parser
 
     def main(self, args):
@@ -29,8 +32,31 @@ class BolusApp (cli.CommandApp):
         resp = self.exec_request(self.pump, query, args=kwds, dryrun=args.dryrun, render_hexdump=False)
         return resp
 
+    def prelude(self, args):
+        port = args.port
+        builder = LinkBuilder()
+        if port == 'scan':
+            port = builder.scan()
+        self.link = link = LinkBuilder().build(args.radio_type, port)
+        link.open()
+        # get link
+        # drain rx buffer
+        self.pump = Pump(self.link, args.serial)
+        if args.no_rf_prelude:
+            return
+        if not args.autoinit:
+            if args.init:
+                self.pump.power_control(minutes=args.session_life)
+        else:
+            self.autoinit(args)
+        self.sniff_model()
 
-def fmt_params (args):
+    def postlude(self, args):
+        # self.link.close( )
+        return
+
+
+def fmt_params(args):
     strokes = int(float(args.units) * args.strokes_per_unit)
     if (args.strokes_per_unit > 10):
         return [lib.HighByte(strokes), lib.LowByte(strokes)]
