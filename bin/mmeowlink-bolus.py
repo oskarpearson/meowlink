8921493337d836b92642e4a9720154834350c1c2
Gustavo <gustavillo@gmail.com>
2016-04-07 23:31:26 -0500

    WIP. Not working.

--- bin/mmeowlink-bolus.py
+++ bin/mmeowlink-bolus.py
@@ -11,12 +11,27 @@ from mmeowlink.handlers.stick import Pump
 
 class BolusApp(cli.CommandApp):
     def customize_parser(self, parser):
-        parser.add_argument('units', type=float, help="Amount of insulin to bolus.")
+        parser.add_argument('units',
+                            type=float,
+                            help="Amount of insulin to bolus."
+                            )
         group = parser.add_mutually_exclusive_group(required=True)
-        group.add_argument('--515', dest='strokes_per_unit', action='store_const', const=10)
-        group.add_argument('--554', dest='strokes_per_unit', action='store_const', const=40)
-        group.add_argument('--strokes', dest='strokes_per_unit', type=int)
-        parser.add_argument('--radio_type', dest='radio_type', default='subg_rfspy', choices=['mmcommander', 'subg_rfspy'])
+        group.add_argument('--515',
+                           dest='strokes_per_unit',
+                           action='store_const',
+                           const=10
+                           )
+        group.add_argument('--554',
+                           dest='strokes_per_unit',
+                           action='store_const',
+                           const=40
+                           )
+        group.add_argument('--strokes',
+                           dest='strokes_per_unit',
+                           type=int
+                           )
+        parser.add_argument('--radio_type', dest='radio_type', default='subg_rfspy',
+                            choices=['mmcommander', 'subg_rfspy'])
         parser.add_argument('--mmcommander', dest='radio_type', action='store_const', const='mmcommander')
         parser.add_argument('--subg_rfspy', dest='radio_type', action='store_const', const='subg_rfspy')
 
@@ -64,5 +79,5 @@ def fmt_params(args):
 
 
 if __name__ == '__main__':
-    app = BolusApp( )
+    app = BolusApp()
     app.run(None)
