29c78cdaa88f0ca14600f93c419820e5b09bbed7
Gustavo <gustavillo@gmail.com>
2016-04-07 23:08:31 -0500

    WIP. Not working.

--- bin/mmeowlink-bolus.py
+++ bin/mmeowlink-bolus.py
@@ -38,7 +38,7 @@ class BolusApp (cli.CommandApp):
                         type=int
                       )
 
-    parser = super(BolusApp, self).customize_parser(parser)
+    # parser = super(BolusApp, self).customize_parser(parser)
     return parser
   def main (self, args):
     print args
