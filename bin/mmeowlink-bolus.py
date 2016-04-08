7bfd5f08c5a44f1f333df0ef6029b1502e8b6e25
Gustavo <gustavillo@gmail.com>
2016-04-07 23:21:38 -0500

    WIP. Not working.

--- bin/mmeowlink-bolus.py
+++ bin/mmeowlink-bolus.py
@@ -7,27 +7,27 @@ from decocare.helpers import cli
 
 
 class BolusApp (cli.CommandApp):
+    def customize_parser(self, parser):
+        parser.add_argument('--radio_type', dest='radio_type', default='subg_rfspy',
+                            choices=['mmcommander', 'subg_rfspy'])
+        parser.add_argument('--mmcommander', dest='radio_type', action='store_const', const='mmcommander')
+        parser.add_argument('--subg_rfspy', dest='radio_type', action='store_const', const='subg_rfspy')
+        parser.add_argument('units', type=float, help="Amount of insulin to bolus.")
+        group = parser.add_mutually_exclusive_group(required=True)
+        group.add_argument('--515', dest='strokes_per_unit', action='store_const', const=10)
+        group.add_argument('--554', dest='strokes_per_unit', action='store_const', const=40)
+        group.add_argument('--strokes', dest='strokes_per_unit', type=int)
+        return parser
 
-  def customize_parser (self, parser):
-    parser.add_argument('--radio_type', dest='radio_type', default='subg_rfspy', choices=['mmcommander', 'subg_rfspy'])
-    parser.add_argument('--mmcommander', dest='radio_type', action='store_const', const='mmcommander')
-    parser.add_argument('--subg_rfspy', dest='radio_type', action='store_const', const='subg_rfspy')
-    parser.add_argument('units', type=float, help="Amount of insulin to bolus.")
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--515', dest='strokes_per_unit', action='store_const', const=10)
-    group.add_argument('--554', dest='strokes_per_unit', action='store_const', const=40)
-    group.add_argument('--strokes', dest='strokes_per_unit', type=int)
-    return parser
+    def main(self, args):
+        print args
+        self.bolus(args);
 
-  def main (self, args):
-    print args
-    self.bolus(args);
-
-  def bolus (self, args):
-    query = commands.Bolus
-    kwds = dict(params=fmt_params(args))
-    resp = self.exec_request(self.pump, query, args=kwds, dryrun=args.dryrun, render_hexdump=False)
-    return resp
+    def bolus(self, args):
+        query = commands.Bolus
+        kwds = dict(params=fmt_params(args))
+        resp = self.exec_request(self.pump, query, args=kwds, dryrun=args.dryrun, render_hexdump=False)
+        return resp
 
 
 def fmt_params (args):
