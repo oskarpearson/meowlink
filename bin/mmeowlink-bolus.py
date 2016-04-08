1cfbe36de46b64c49d812436a130c0c0bdcca603
Gustavo <gustavillo@gmail.com>
2016-04-07 23:20:27 -0500

    WIP. Not working.

--- bin/mmeowlink-bolus.py
+++ bin/mmeowlink-bolus.py
@@ -5,41 +5,20 @@ from decocare import commands
 from decocare import lib
 from decocare.helpers import cli
 
+
 class BolusApp (cli.CommandApp):
-  """ %(prog)s - Send bolus command to a pump.
 
-  XXX: Be careful please!
-
-  Units might be wrong.  Keep disconnected from pump until you trust it by
-  observing the right amount first.
-  """
   def customize_parser (self, parser):
     parser.add_argument('--radio_type', dest='radio_type', default='subg_rfspy', choices=['mmcommander', 'subg_rfspy'])
     parser.add_argument('--mmcommander', dest='radio_type', action='store_const', const='mmcommander')
     parser.add_argument('--subg_rfspy', dest='radio_type', action='store_const', const='subg_rfspy')
-    parser.add_argument('units',
-                         type=float,
-                         help="Amount of insulin to bolus."
-                       )
-
+    parser.add_argument('units', type=float, help="Amount of insulin to bolus.")
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('--515',
-                        dest='strokes_per_unit',
-                        action='store_const',
-                        const=10
-                      )
-    group.add_argument('--554',
-                        dest='strokes_per_unit',
-                        action='store_const',
-                        const=40
-                      )
-    group.add_argument('--strokes',
-                        dest='strokes_per_unit',
-                        type=int
-                      )
-
-    # parser = super(BolusApp, self).customize_parser(parser)
+    group.add_argument('--515', dest='strokes_per_unit', action='store_const', const=10)
+    group.add_argument('--554', dest='strokes_per_unit', action='store_const', const=40)
+    group.add_argument('--strokes', dest='strokes_per_unit', type=int)
     return parser
+
   def main (self, args):
     print args
     self.bolus(args);
@@ -47,17 +26,17 @@ class BolusApp (cli.CommandApp):
   def bolus (self, args):
     query = commands.Bolus
     kwds = dict(params=fmt_params(args))
-
-    resp = self.exec_request(self.pump, query, args=kwds,
-                 dryrun=args.dryrun, render_hexdump=False)
+    resp = self.exec_request(self.pump, query, args=kwds, dryrun=args.dryrun, render_hexdump=False)
     return resp
 
+
 def fmt_params (args):
-  strokes = int(float(args.units) * args.strokes_per_unit)
-  if (args.strokes_per_unit > 10):
-    return [lib.HighByte(strokes), lib.LowByte(strokes)]
-  return [strokes]
+    strokes = int(float(args.units) * args.strokes_per_unit)
+    if (args.strokes_per_unit > 10):
+        return [lib.HighByte(strokes), lib.LowByte(strokes)]
+    return [strokes]
+
 
 if __name__ == '__main__':
-  app = BolusApp( )
-  app.run(None)
+    app = BolusApp( )
+    app.run(None)
