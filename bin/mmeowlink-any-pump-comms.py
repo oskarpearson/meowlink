e0ce20bef481f9801d0bf4f4c4c3cbb6234ef5f4
Dana Lewis <danamlewis@users.noreply.github.com>
2017-05-15 12:44:01 -0700

    Further attempting to clarify the pump comms message

--- bin/mmeowlink-any-pump-comms.py
+++ bin/mmeowlink-any-pump-comms.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
 
   # app.run doesn't return the call status, so we need to interrogate the object:
   if app.app_result == 0:
-    print("No pump comms detected from other rigs (this is a good thing!)")
+    print("No interfering pump comms detected from other rigs (this is a good thing!)")
   else:
     print("Comms with pump detected")
 
